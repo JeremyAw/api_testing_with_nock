b30f5430c2c03fb9127d4bd5935ca30cf879894a
Jeremy Aw <jeremyinelysium@gmail.com>
2020-12-21 01:03:15 +0800

    Use custom router in test server
    - Allow JSON payloads
    - Allow URL encoding payloads

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const test_router = require('./test_router.js')
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/', test_router)
 
 app.listen(port, () => {
   console.log(`Test server is listening at http://localhost:${port}`)
